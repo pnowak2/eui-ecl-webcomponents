5dbd4203e6167a77965e870d1489a8d7f808b28d
Piotr Nowak <p.nowak2@gmail.com>
2019-10-06 12:11:03 +0200

    just dist

---
+++ build/p-hbsmdetb.entry.js
@@ -0,0 +1,1 @@
+import{r as s,h as t}from"./p-a245287f.js";const r=class{constructor(t){s(this,t)}render(){return t("div",null)}static get style(){return""}};export{r as ecl_svg_definitions};
--- www/build/eui-webcomponents.esm.js
+++
@@ -1,6 +0,0 @@
-import { p as patchBrowser, g as globals, b as bootstrapLazy } from './core-9eaea7c8.js';
-
-patchBrowser().then(options => {
-  globals();
-  return bootstrapLazy([["ecl-message",[[4,"ecl-message",{"styleClass":[1,"style-class"],"typeClass":[1,"type-class"],"title":[1]}]]],["ecl-showcase",[[0,"ecl-showcase"]]],["ecl-svg-definitions",[[0,"ecl-svg-definitions"]]],["ecl-expandable",[[4,"ecl-expandable",{"styleClass":[1,"style-class"],"isExpanded":[4,"is-expanded"]}]]],["ecl-accordion-item",[[4,"ecl-accordion-item",{"styleClass":[1,"style-class"],"label":[1],"isExpanded":[4,"is-expanded"]}]]],["ecl-tag",[[4,"ecl-tag",{"styleClass":[1,"style-class"],"isRemovable":[4,"is-removable"]}]]],["ecl-accordion",[[4,"ecl-accordion",{"styleClass":[1,"style-class"]}]]],["ecl-button",[[4,"ecl-button",{"styleClass":[1,"style-class"],"type":[1]}]]],["ecl-icon",[[0,"ecl-icon",{"styleClass":[1,"style-class"],"icon":[1],"size":[1],"color":[1],"transform":[1]}]]]], options);
-});
